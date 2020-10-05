472bf3bb657766d5be6e5ae5e37860a9a0f253bd
HiDraqula <aniketaz190@gmail.com>
2020-10-05 13:41:05 +0530

    New

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Mywebsite
 
+Live Here... https://myselfani.netlify.app/
+
+##### And A Little Philosophy...
+
+`If something is free why to waste money on it`,
+
+i'll rather choose to enjoy a favrouite meal with my beloved ones...😇✌
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.3.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
